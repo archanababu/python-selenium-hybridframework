be76771415331d24173eb72bb3c23902f69f49b1
arcbabu <archana.masterseducation@gmail.com>
2022-04-22 11:10:06 -0700

    created batch file to run test cases directly from windows

---
+++ run.bat
@@ -0,0 +1,4 @@
+pytest -s -v -m "regression" --html=Reports\report.html TestCases\
+REM pytest -s -v -m "sanity" --html=Reports\report.html TestCases\
+REM pytest -s -v -m "sanity or regression" --html=Reports\report.html TestCases
+REM pytest -s -v -m "sanity and regression" --html=Reports\report.html TestCases\
